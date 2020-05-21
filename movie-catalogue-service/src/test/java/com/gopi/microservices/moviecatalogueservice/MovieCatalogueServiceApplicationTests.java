bfe45ade29559a26d9a575198e94c6289148b068
gopinath_mb <gopinathjnv@gmail.com>
2020-05-21 22:05:31 +0530

    Adding movie-catalogue-service microservice

---
+++ movie-catalogue-service/src/test/java/com/gopi/microservices/moviecatalogueservice/MovieCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopi.microservices.moviecatalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
